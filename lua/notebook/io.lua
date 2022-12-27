147cc5d9c0b1e9a8e9b5ce0f6d644cdfd9d07aae
Owen Campbell <owen.campbell@tanti.org.uk>
2022-12-27 12:11:56 +0000

    Add default notebook

--- lua/notebook/io.lua
+++ lua/notebook/io.lua
@@ -3,12 +3,27 @@
 -- This software is published at https://github.com/meatballs/notebook.nvim
 
 local M = {}
+local DEFAULT_NOTEBOOK = {
+    cells = {},
+    metadata = {
+        language_info = {
+            name = "python"
+        },
+        nbformat = 4,
+        nbformat_minor = 5,
+    }
+}
 
 M.parse_ipynb = function(buffer)
     local lines = vim.api.nvim_buf_get_lines(buffer, 0, -1, true)
     local content = table.concat(lines, "")
     content = content:gsub("\n", "")
-    return vim.json.decode(content)
+    local parsed_ok, result = pcall(vim.json.decode, content)
+    if parsed_ok then
+        return result
+    else
+        return DEFAULT_NOTEBOOK
+    end
 end
 
 return M
