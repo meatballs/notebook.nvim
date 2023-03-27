ac183f2c9b7976ca94da94d3ee418f5395d79d77
Owen Campbell <owen.campbell@tanti.org.uk>
2023-03-27 10:53:13 +0100

    Add fix for missing language_info in metadata

--- lua/notebook.lua
+++ lua/notebook.lua
@@ -15,11 +15,15 @@ local set_language = function(buffer, content, settings)
     vim.ui.select({ "python", "r", "julia" }, {
         prompt = "Select language:",
     }, function(choice)
+        if not content.metadata.language_info then
+            content.metadata.language_info = {}
+        end
         content.metadata.language_info.name = choice
         render.notebook(buffer, content, settings)
     end
     )
 end
+
 
 M.read_notebook = function(autocmd)
     local buffer = autocmd.buf
@@ -38,7 +42,8 @@ M.read_notebook = function(autocmd)
     vim.api.nvim_buf_create_user_command(buffer, "NBAddCell", commands.add_cell, {nargs = "?"})
     vim.api.nvim_buf_create_user_command(buffer, "NBInsertCell", commands.insert_cell, { nargs = "?" })
     vim.api.nvim_buf_create_user_command(buffer, "NBDeleteCell", commands.delete_cell, { nargs = "?" })
-    if #content.metadata.language_info.name > 0 then
+    local language_info = content.metadata.language_info
+    if language_info and language_info.name then
         render.notebook(buffer, content, settings)
     else
         set_language(buffer, content, settings)
@@ -51,9 +56,11 @@ end
 M.write_notebook = function(autocmd)
     local content = ipynb.dump(autocmd.buf)
     local file = io.open(autocmd.file, "w")
-    file:write(content)
-    file.close()
-    vim.api.nvim_buf_set_option(autocmd.buf, "modified", false)
+    if file then
+        file:write(content)
+        file:close()
+        vim.api.nvim_buf_set_option(autocmd.buf, "modified", false)
+    end
 end
 
 vim.api.nvim_create_autocmd({ "BufRead" }, {
